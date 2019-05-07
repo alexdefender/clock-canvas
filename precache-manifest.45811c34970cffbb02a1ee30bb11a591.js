8ccdf46795ad91482728bb4237db4525f0380c9d
Alex <alexiv1102@gmail.com>
2019-05-07 10:19:44 +0300

    Updates

---
+++ precache-manifest.45811c34970cffbb02a1ee30bb11a591.js
@@ -0,0 +1,14 @@
+self.__precacheManifest = [
+  {
+    "revision": "e760501c5d4d72a9c362",
+    "url": "/clock-canvas/static/js/runtime~main.62360b56.js"
+  },
+  {
+    "revision": "bd3b4e7ea39efb243422",
+    "url": "/clock-canvas/static/js/main.e1d35311.chunk.js"
+  },
+  {
+    "revision": "2ad062a502b274c6ef9b8d1bba59e3f6",
+    "url": "/clock-canvas/index.html"
+  }
+];
